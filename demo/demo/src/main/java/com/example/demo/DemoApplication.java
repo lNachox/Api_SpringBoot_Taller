f45193927ecb0dbc42351f50322fec00592aeeb8
Ignacio Manríquez <i.manriquez01@ufromail.cl>
2024-06-05 16:27:40 -0400

    Commit v0.2
    update files

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,43 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.models.campeon;
+import com.example.demo.models.counter;
+import com.example.demo.models.especie;
+import com.example.demo.models.interaccion;
+import com.example.demo.models.rival;
+
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class DemoApplication {
+	@Autowired
+    private counter counterRepository;
 
-	public static void main(String[] args) {
+    @Autowired
+    private campeon campeonRepository;
+
+    @Autowired
+    private especie especieRepository;
+
+    @Autowired
+    private rival rivalRepository;
+
+    @Autowired
+    private interaccion interaccionRepository;
+
+    @Autowired
+    private campeonhasinteraccion campeonHasInteraccionRepository;
+
+    public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+    @PostConstruct
+    public void generateData() {
+        
+    }
 }
